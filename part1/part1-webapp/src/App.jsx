17db03bd24dd2f3f585ab0b6be704c65c8729c9c
Pedro Tanamachi <pedroeduardotanamachigarza@gmail.com>
2025-03-12 16:13:44 -0700

    Exercises 1.1 finished

---
+++ part1/part1-webapp/src/App.jsx
@@ -0,0 +1,40 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Content = (props) => {
+  return (
+    <>
+      {props.partsAndExersices.map((e) => (
+        <p>
+          {e.part} {e.exercises}
+        </p>
+      ))}
+    </>
+  )
+}
+
+const Total = (props) => {
+  return <p>Number of exercises {props.total}</p>
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const partsAndExersices = [
+    { part: 'Fundamentals of React', exercises: 10 },
+    { part: 'Using props to pass data', exercises: 7 },
+    { part: 'State of a component', exercises: 14 },
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content partsAndExersices={partsAndExersices} />
+      <Total
+        total={partsAndExersices.reduce((acc, e) => acc + e.exercises, 0)}
+      />
+    </div>
+  )
+}
+
+export default App
