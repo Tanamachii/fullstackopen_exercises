51772f2d8e27c18e792934d44e032a2400c69bb4
Pedro Tanamachi <pedroeduardotanamachigarza@gmail.com>
2025-03-12 16:31:42 -0700

    Exercises 1.5 finished

--- part1/part1-webapp/src/App.jsx
+++ part1/part1-webapp/src/App.jsx
@@ -25,18 +25,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 },
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total total={parts.reduce((acc, e) => acc + e.exercises, 0)} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total total={course.parts.reduce((acc, e) => acc + e.exercises, 0)} />
     </div>
   )
 }
